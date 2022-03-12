d87ad7481f1066afcddb69d5bd42ae905d888b56
mohaa <mohaa.r.6789@gmail.com>
2022-03-13 01:28:10 +0530

    dashboard comp included in the dashboard grid

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,18 @@
-import Header from '@components/Header'
 import Box from '@mui/material/Box'
+import Header from '@components/Header'
+import DashboardGrid from '@components/DashboardGrid'
+import Container from '@mui/material/Container'
+import { useRouter } from 'next/router'
 
 function Dashboard() {
   return (
     <Box>
       <Header />
+      <Container>
+        <Box sx={{ display: 'flex' }}>
+          <DashboardGrid />
+        </Box>
+      </Container>
     </Box>
   )
 }
