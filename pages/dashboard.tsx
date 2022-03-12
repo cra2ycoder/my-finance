556d640660215c6acd90d085edb920132ece4c80
mohaa <mohaa.r.6789@gmail.com>
2022-03-13 00:46:21 +0530

    dashboard updated with ui

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,21 @@
+import Header from '@components/Header'
+import Box from '@mui/material/Box'
+
 function Dashboard() {
-  return <>dashboard</>
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.dark',
+        height: 64,
+        alignItems: 'center',
+        display: 'flex',
+        p: 2,
+        boxSizing: 'border-box',
+      }}
+    >
+      <Header />
+    </Box>
+  )
 }
 
 export default Dashboard
