3fa26ce37772944a04341d3ef2ecf106eee385f0
mohaa <mohaa.r.6789@gmail.com>
2022-03-28 20:28:10 +0530

    dashboard grid ui updates

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,16 +2,14 @@ import Box from '@mui/material/Box'
 import Header from '@components/Header'
 import DashboardGrid from '@components/DashboardGrid'
 import Container from '@mui/material/Container'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
 function Dashboard() {
   return (
     <Box>
       <Header />
-      <Container>
-        <Box sx={{ display: 'flex' }}>
-          <DashboardGrid />
-        </Box>
+      <Container sx={{ margin: 'auto', marginTop: '3rem' }}>
+        <DashboardGrid />
       </Container>
     </Box>
   )
