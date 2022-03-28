6968880808b203204fff3dc065efc9a8f39312f1
mohaa <mohaa.r.6789@gmail.com>
2022-03-28 20:28:23 +0530

    dashboard grid updates

--- components/DashboardGrid/DashboardGrid.tsx
+++ components/DashboardGrid/DashboardGrid.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
-// import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import NotificationsIcon from '@mui/icons-material/Notifications'
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange'
+import DiamondIcon from '@mui/icons-material/Diamond'
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet'
 
 function DashboardGridItem(props: any) {
   return (
-    <Grid item sx={{ backgroundColor: '#f0f0f0' }}>
-      <NotificationsIcon sx={{ width: 75, height: 75 }} />
-      <Typography sx={{ color: 'black' }}>{props.title || 'Title'}</Typography>
-      <Typography sx={{ color: 'black' }}>
+    <Box
+      // gridColumn={'span 2'}
+      sx={{
+        backgroundColor: '#f0f0f0',
+        textAlign: 'center',
+        padding: '5rem',
+      }}
+    >
+      {props.icon}
+      <Typography sx={{ color: '#235ad1', fontSize: '2rem', fontWeight: 100 }}>
+        {props.title || 'Title'}
+      </Typography>
+      <Typography
+        sx={{
+          color: '#666666',
+          fontWeight: 100,
+        }}
+      >
         {props.desc || 'desc goes here!'}
       </Typography>
-    </Grid>
+    </Box>
   )
 }
 
@@ -21,31 +37,39 @@ function DashboardGrid(props: any) {
     {
       title: 'Monthly Expences',
       desc: 'commitments/unplanned/savings/pending',
+      icon: (
+        <AccountBalanceWalletIcon
+          sx={{ width: 50, height: 50, fill: '#707b8b' }}
+        />
+      ),
     },
     {
       title: 'Investments',
       desc: 'Overall mutual funds/stocks/returns/SIP',
+      icon: (
+        <CurrencyExchangeIcon sx={{ width: 50, height: 50, fill: '#707b8b' }} />
+      ),
     },
     {
       title: 'Gold Savings',
       desc: 'Total golds/gold chit',
+      icon: <DiamondIcon sx={{ width: 50, height: 50, fill: '#707b8b' }} />,
     },
     {
       title: 'Loans',
       desc: 'Personal/Plot/Home/Gold loans and EMIs',
+      icon: (
+        <AccountBalanceIcon sx={{ width: 50, height: 50, fill: '#707b8b' }} />
+      ),
     },
   ]
 
   return (
-    <Grid
-      container
-      //   spacing={{ xs: 2, md: 2, lg: 2 }}
-      //   columns={{ xs: 2, sm: 2, md: 2, lg: 2 }}
-    >
-      {list.map(x => (
-        <DashboardGridItem {...x} />
+    <Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={2}>
+      {list.map((x, idx) => (
+        <DashboardGridItem {...x} key={`grid-item-${idx}`} />
       ))}
-    </Grid>
+    </Box>
   )
 }
 
