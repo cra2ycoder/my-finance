d8a3e6460465df45538c97c235b91f1de2637239
mohaa <mohaa.r.6789@gmail.com>
2022-03-29 00:21:04 +0530

    removed header from dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,13 +1,10 @@
 import Box from '@mui/material/Box'
-import Header from '@components/Header'
 import DashboardGrid from '@components/DashboardGrid'
 import Container from '@mui/material/Container'
-// import { useRouter } from 'next/router'
 
 function Dashboard() {
   return (
     <Box>
-      <Header />
       <Container sx={{ margin: 'auto', marginTop: '3rem' }}>
         <DashboardGrid />
       </Container>
