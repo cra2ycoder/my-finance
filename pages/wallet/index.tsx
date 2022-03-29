efda22c9f3360467208b58b6df45c05de9ac755f
mohaa <mohaa.r.6789@gmail.com>
2022-03-29 22:07:19 +0530

    wallet page created

---
+++ pages/wallet/index.tsx
@@ -0,0 +1,45 @@
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import WalletTable from '@components/WalletTable'
+// import MonthsGrid from '@components/MonthsGrid'
+import { walletItems } from '@model/config/data'
+import YearPicker from '@components/YearPicker'
+
+function Wallet() {
+  return (
+    <Box>
+      <Container sx={{ margin: 'auto' }}>
+        <Box display="flex" justifyContent="space-between">
+          <Typography
+            sx={{
+              color: '#235ad1',
+              fontSize: '2.5vh',
+              fontWeight: 100,
+              margin: '2rem 0',
+            }}
+          >
+            <span>Available: </span>
+            <span>Rs. 1,400.00</span>
+          </Typography>
+          <Typography
+            sx={{
+              color: '#235ad1',
+              fontSize: '2.5vh',
+              fontWeight: 100,
+              margin: '2rem 0',
+            }}
+          >
+            <span>Total Income: </span>
+            <span>Rs. 1,23,400.00</span>
+          </Typography>
+        </Box>
+        <YearPicker />
+        {/* <MonthsGrid /> */}
+        <WalletTable list={walletItems} />
+      </Container>
+    </Box>
+  )
+}
+
+export default Wallet
