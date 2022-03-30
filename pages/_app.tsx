67d4557034ddf298685ce43f12a2d582619a3b38
mohaa <mohaa.r.6789@gmail.com>
2022-03-30 22:40:54 +0530

    page items integrated dynamically

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { pageItems } from '@model/config'
 import '../styles/global.css'
 
 const theme = createTheme({
@@ -26,30 +27,33 @@ const theme = createTheme({
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const componentName = Component.name
+  const { title = '', desc = '' } = pageItems[componentName.toLowerCase()] || {}
+
   return (
     <ThemeProvider theme={theme}>
       <Header />
       <Box marginTop="6rem">
-        <Container sx={{ margin: 'auto' }}>
-          <Box>
+        <Container sx={{ margin: 'auto', display: 'flex', flexWrap: 'wrap' }}>
+          <Box width="50%" className="page-title">
             <Typography
               sx={{
                 fontSize: '4.5vh',
-                margin: '2rem 0',
                 fontWeight: 800,
+                color: 'rgb(19, 47, 76)',
               }}
             >
-              {Component.name || 'Component Name'}
+              {title}
             </Typography>
             <Typography
               sx={{
-                fontSize: '1.5vh',
-                margin: '2rem 0',
-                fontWeight: 400,
+                fontSize: '2vh',
+                fontWeight: 300,
+                marginBottom: '2rem',
                 color: 'rgb(62, 80, 96)',
               }}
             >
-              Where your all spendings will be maintained here.
+              {desc}
             </Typography>
           </Box>
           <Component {...pageProps} />
