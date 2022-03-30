1111d32101b61312657b7a5587214a6bc38008be
mohaa <mohaa.r.6789@gmail.com>
2022-03-30 17:59:04 +0530

    wallet page updates

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,5 +1,4 @@
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import WalletTable from '@components/WalletTable'
 import { walletItems } from '@model/config/data'
@@ -7,41 +6,35 @@ import YearPicker from '@components/YearPicker'
 
 function Wallet() {
   return (
-    <Box>
-      <Container sx={{ margin: 'auto' }}>
-        <Box
-          display="flex"
-          flexWrap="wrap"
-          justifyContent="space-between"
-          sx={{ borderBottom: '1px solid #7b1ea2', paddingBottom: '2rem' }}
+    <>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="space-between"
+        sx={{ borderBottom: '1px solid #7b1ea2', paddingBottom: '2rem' }}
+      >
+        <Typography
+          sx={{
+            color: '#235ad1',
+            fontSize: '2.5vh',
+          }}
         >
-          <Typography
-            sx={{
-              color: '#235ad1',
-              fontSize: '2.5vh',
-              fontWeight: 100,
-              margin: '2rem 0',
-            }}
-          >
-            <span>Available: </span>
-            <span>Rs. 1,400.00</span>
-          </Typography>
-          <Typography
-            sx={{
-              color: '#235ad1',
-              fontSize: '2.5vh',
-              fontWeight: 100,
-              margin: '2rem 0',
-            }}
-          >
-            <span>Total Income: </span>
-            <span>Rs. 1,23,400.00</span>
-          </Typography>
-          <YearPicker />
-        </Box>
-        <WalletTable list={walletItems} />
-      </Container>
-    </Box>
+          <span>Available: </span>
+          <span>Rs. 1,400.00</span>
+        </Typography>
+        <Typography
+          sx={{
+            color: '#235ad1',
+            fontSize: '2.5vh',
+          }}
+        >
+          <span>Total Income: </span>
+          <span>Rs. 1,23,400.00</span>
+        </Typography>
+        <YearPicker />
+      </Box>
+      <WalletTable list={walletItems} />
+    </>
   )
 }
 
