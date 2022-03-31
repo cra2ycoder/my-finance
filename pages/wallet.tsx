57b0d1444da05c301b1bc5d01b3eea779b5c0cf2
mohaa <mohaa.r.6789@gmail.com>
2022-04-01 00:03:43 +0530

    wallet ui change

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,8 +1,27 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import WalletTable from '@components/WalletTable'
 import { walletItems } from '@model/config/data'
 
 function Wallet() {
-  return <WalletTable list={walletItems} />
+  return (
+    <Box width="100%">
+      <Typography
+        sx={{
+          fontSize: '2rem',
+          width: '100%',
+          fontWeight: 600,
+          color: '#0179f3',
+          textAlign: 'right',
+          marginBottom: '1rem',
+        }}
+      >
+        <span>Total: </span>
+        <span>Rs. 1,23,400.00</span>
+      </Typography>
+      <WalletTable list={walletItems} />
+    </Box>
+  )
 }
 
 export default Wallet
