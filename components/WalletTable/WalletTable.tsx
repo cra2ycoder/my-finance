0fce67f927657cb03bcdf41d0f28f328cc76772c
mohaa <mohaa.r.6789@gmail.com>
2022-04-01 00:03:35 +0530

    wallet table ui short

--- components/WalletTable/WalletTable.tsx
+++ components/WalletTable/WalletTable.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
 import WalletItem from './WalletItem'
 import { IWalletTableProps, IWalletItemProps } from './typings'
 
@@ -9,36 +7,11 @@ function WalletTable(props: IWalletTableProps) {
   const { list = [] } = props
 
   return (
-    <Box width="100%">
-      <Typography
-        sx={{
-          fontSize: '1.5rem',
-          width: '100%',
-          fontWeight: 600,
-          color: '#0179f3',
-          textAlign: 'right',
-        }}
-      >
-        <span>Total Income: </span>
-        <span>Rs. 1,23,400.00</span>
-      </Typography>
-
-      <Paper
-        sx={{ borderRadius: '10px', width: '100%', marginTop: '1.5rem' }}
-        elevation={1}
-      >
-        <Box
-          className="wallet-table"
-          sx={{
-            width: '100%',
-          }}
-        >
-          {list.map((x: IWalletItemProps, id: number) => (
-            <WalletItem {...x} key={`wallet-item-${id}`} />
-          ))}
-        </Box>
-      </Paper>
-    </Box>
+    <>
+      {list.map((x: IWalletItemProps, id: number) => (
+        <WalletItem {...x} key={`wallet-item-${id}`} />
+      ))}
+    </>
   )
 }
 
