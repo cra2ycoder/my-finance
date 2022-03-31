78a7580962fe21b06a9378ec847ba9bf558b5508
mohaa <mohaa.r.6789@gmail.com>
2022-03-31 23:49:46 +0530

    wallet table comes with total value as well

--- components/WalletTable/WalletTable.tsx
+++ components/WalletTable/WalletTable.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
 import WalletItem from './WalletItem'
 import { IWalletTableProps, IWalletItemProps } from './typings'
 
@@ -8,21 +9,36 @@ function WalletTable(props: IWalletTableProps) {
   const { list = [] } = props
 
   return (
-    <Paper
-      sx={{ borderRadius: '10px', width: '100%', marginTop: '1.5rem' }}
-      elevation={1}
-    >
-      <Box
-        className="wallet-table"
+    <Box width="100%">
+      <Typography
         sx={{
+          fontSize: '1.5rem',
           width: '100%',
+          fontWeight: 600,
+          color: '#0179f3',
+          textAlign: 'right',
         }}
       >
-        {list.map((x: IWalletItemProps, id: number) => (
-          <WalletItem {...x} key={`wallet-item-${id}`} />
-        ))}
-      </Box>
-    </Paper>
+        <span>Total Income: </span>
+        <span>Rs. 1,23,400.00</span>
+      </Typography>
+
+      <Paper
+        sx={{ borderRadius: '10px', width: '100%', marginTop: '1.5rem' }}
+        elevation={1}
+      >
+        <Box
+          className="wallet-table"
+          sx={{
+            width: '100%',
+          }}
+        >
+          {list.map((x: IWalletItemProps, id: number) => (
+            <WalletItem {...x} key={`wallet-item-${id}`} />
+          ))}
+        </Box>
+      </Paper>
+    </Box>
   )
 }
 
