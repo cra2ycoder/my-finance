7a8ea43cb6c9aee0544ad10fba9368ff2dedc5d9
mohaa <mohaa.r.6789@gmail.com>
2022-04-01 23:59:22 +0530

    app cleaned with latest ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { pageItems } from '@model/config'
 import '../styles/global.css'
 
 const theme = createTheme({
@@ -27,39 +23,13 @@ const theme = createTheme({
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  const componentName = Component.name
-  const { title = '', desc = '' } = pageItems[componentName.toLowerCase()] || {}
-
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      <Box paddingTop="6rem">
-        <Container sx={{ margin: 'auto', display: 'flex', flexWrap: 'wrap' }}>
-          <Box width="50%" className="page-title">
-            <Typography
-              sx={{
-                fontSize: '4.5vh',
-                fontWeight: 700,
-                color: 'rgb(19, 47, 76)',
-              }}
-            >
-              {title}
-            </Typography>
-            <Typography
-              sx={{
-                fontSize: '2vh',
-                fontWeight: 300,
-                marginBottom: '2rem',
-                color: 'rgb(62, 80, 96)',
-              }}
-            >
-              {desc}
-            </Typography>
-          </Box>
-          <Component {...pageProps} />
-        </Container>
+      <main className="main-app-wrapper">
+        <Component {...pageProps} />
         <Footer />
-      </Box>
+      </main>
     </ThemeProvider>
   )
 }
