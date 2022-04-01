f8767407e9fcd3dd72b74f659cdae60cae2b8b23
mohaa <mohaa.r.6789@gmail.com>
2022-04-02 00:37:43 +0530

    pagetitle ui updates

--- components/PageTitle/PageTitle.tsx
+++ components/PageTitle/PageTitle.tsx
@@ -4,28 +4,37 @@ import Typography from '@mui/material/Typography'
 
 function PageTitle(props: any) {
   const { title, desc, children } = props
+
   return (
-    <Box width="50%" className="page-title">
-      <Typography
-        sx={{
-          fontSize: '4.5vh',
-          fontWeight: 700,
-          color: 'rgb(19, 47, 76)',
-        }}
-      >
-        {title}
-      </Typography>
-      <Typography
-        sx={{
-          fontSize: '2vh',
-          fontWeight: 300,
-          marginBottom: '2rem',
-          color: 'rgb(62, 80, 96)',
-        }}
-      >
-        {desc}
-      </Typography>
-      {children}
+    <Box
+      width="100%"
+      className="page-title"
+      display="flex"
+      justifyContent="space-between"
+      marginBottom="1rem"
+    >
+      <Box>
+        <Typography
+          sx={{
+            fontSize: '4.5vh',
+            fontWeight: 700,
+            color: 'rgb(19, 47, 76)',
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: '2vh',
+            fontWeight: 300,
+            marginBottom: '2rem',
+            color: 'rgb(62, 80, 96)',
+          }}
+        >
+          {desc}
+        </Typography>
+      </Box>
+      <Box>{children}</Box>
     </Box>
   )
 }
