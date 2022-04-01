b69950019174235b337004bc891a3fc5b2adbe2b
mohaa <mohaa.r.6789@gmail.com>
2022-04-01 23:58:36 +0530

    lengthy page name files removed

--- pages/monthly-expenses.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MonthlySpentList from '@components/MonthlySpentList'
-import { monthlySpentList } from '@model/config'
-
-function MonthlyExpenses() {
-  return <MonthlySpentList list={monthlySpentList} />
-}
-
-export default MonthlyExpenses
