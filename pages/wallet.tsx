5ccf3176b2b3cecbbc9a4116a7978c6fd2941773
mohaa <mohaa.r.6789@gmail.com>
2022-04-02 00:00:04 +0530

    wallet page ui cleanup

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,24 +1,27 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import WalletTable from '@components/WalletTable'
-import { walletItems } from '@model/config/data'
+import PageTitle from '@components/PageTitle'
+import { walletItems, pageItems } from '@model/config'
 
 function Wallet() {
   return (
     <Box width="100%">
-      <Typography
-        sx={{
-          fontSize: '2rem',
-          width: '100%',
-          fontWeight: 600,
-          color: '#0179f3',
-          textAlign: 'right',
-          marginBottom: '1rem',
-        }}
-      >
-        <span>Total: </span>
-        <span>Rs. 1,23,400.00</span>
-      </Typography>
+      <PageTitle {...pageItems.wallet}>
+        <Typography
+          sx={{
+            fontSize: '2rem',
+            width: '100%',
+            fontWeight: 600,
+            color: '#0179f3',
+            textAlign: 'right',
+            marginBottom: '1rem',
+          }}
+        >
+          <span>Total: </span>
+          <span>Rs. 1,23,400.00</span>
+        </Typography>
+      </PageTitle>
       <WalletTable list={walletItems} />
     </Box>
   )
