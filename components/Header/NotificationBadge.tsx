28ea6ce94cf7c70bdca844f4f3aaed18e7fb44ef
mohaa <mohaa.r.6789@gmail.com>
2022-04-04 04:09:28 +0530

    notification badge cleaned

--- components/Header/NotificationBadge.tsx
+++ components/Header/NotificationBadge.tsx
@@ -4,6 +4,7 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Badge from '@mui/material/Badge'
 import NotificationsIcon from '@mui/icons-material/Notifications'
+import menuPageProps from './props'
 
 function NotificationBadge() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
@@ -33,38 +34,7 @@ function NotificationBadge() {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        PaperProps={{
-          elevation: 6,
-          sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 4px 4px rgba(0,0,0,0.20))',
-            mt: 1.5,
-            '&.MuiAvatar-root': {
-              ml: -0.5,
-              mr: 1,
-            },
-            '&.MuiPaper-root': {
-              width: '360px',
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 26,
-              width: 10,
-              height: 10,
-              backgroundColor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        {...menuPageProps}
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
