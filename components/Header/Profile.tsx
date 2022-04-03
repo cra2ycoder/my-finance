6a54b1bb893ae5466f9708456f8886852b3c7040
mohaa <mohaa.r.6789@gmail.com>
2022-04-04 04:09:35 +0530

    profile code clean

--- components/Header/Profile.tsx
+++ components/Header/Profile.tsx
@@ -4,9 +4,9 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import Avatar from '@mui/material/Avatar'
 import Stack from '@mui/material/Stack'
-import Popover from '@mui/material/Popover'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import { deepPurple } from '@mui/material/colors'
+import menuPageProps from './props'
 
 function Profile() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
@@ -38,39 +38,7 @@ function Profile() {
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: 'visible',
-            mt: 1.5,
-            '&.MuiAvatar-root': {
-              ml: -0.5,
-              mr: 1,
-            },
-            '&.MuiPaper-root': {
-              width: '200px',
-              boxShadow:
-                'rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) -20px 20px 40px -4px',
-            },
-            '&:before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 26,
-              width: 10,
-              height: 10,
-              backgroundColor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        {...menuPageProps}
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={handleClose}>My account</MenuItem>
