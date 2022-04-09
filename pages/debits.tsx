ce5cd5cafea0f1ac9f5edf913cc4e5f9267ad311
mohaa <mohaa.r.6789@gmail.com>
2022-04-09 23:37:26 +0530

    debits page updated

--- pages/debits.tsx
+++ pages/debits.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
-import DebitList from '@components/DebitList'
 import PageTitle from '@components/PageTitle'
+import MoneyBox, { MoneyBoxHolder } from '@components/MoneyBox'
 import { debitList, pageItems } from '@model/config'
 
 function MonthlyExpenses() {
+  const filters = [
+    {
+      type: 'unplanned',
+      title: 'Unplanned',
+    },
+    {
+      type: 'commitment',
+      title: 'Commitments',
+    },
+    {
+      type: 'savings',
+      title: 'Savings',
+    },
+  ]
+
   return (
     <>
       <PageTitle {...pageItems.debits}></PageTitle>
-      <DebitList list={debitList} />
+      <MoneyBoxHolder>
+        {filters.map(x => (
+          <MoneyBox list={debitList} {...x} />
+        ))}
+      </MoneyBoxHolder>
     </>
   )
 }
