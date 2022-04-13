bba4213e9e37f8a666c7bc574b47ab8bcdc2a606
mohaa <mohaa.r.6789@gmail.com>
2022-04-14 01:32:34 +0530

    dashboard updates
    - moneytable added
    - grand total added

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,38 +1,55 @@
 import Box from '@mui/material/Box'
 import PageTitle from '@components/PageTitle'
+import Typography from '@mui/material/Typography'
 import {
   MonthlyChart,
   DailyChart,
   YearlySavingsChart,
   MonthlyIncomeChart,
 } from '@components/Charts'
-import { Money } from '@components/MoneyBox'
+import { MoneyTable } from '@components/MoneyBox'
 import { pageItems } from '@model/config'
 
 function Dashboard() {
+  const moneyList = [
+    { name: 'A/C Balance', value: '54,835' },
+    { name: 'Total Properties', value: '67,25,000' },
+    { name: 'Total Investments', value: '34,67,291' },
+    { name: 'Total Golds', value: '11,08,128' },
+    { name: 'Total Loans', value: '17,92,000' },
+  ]
+
   return (
     <Box>
-      <PageTitle {...pageItems.dashboard} />
-      <Box
-        className="wallet-info-panel"
-        sx={{
-          display: 'grid',
-          background:
-            'linear-gradient(to right bottom, rgb(18 55 92), rgb(74 30 114) 120%)',
-          marginBottom: '1rem',
-          padding: '4rem 2rem',
-          marginLeft: '-2rem',
-          marginRight: '-2rem',
-          justifyContent: 'space-between',
-          gridTemplateColumns: 'repeat(5, 1fr)',
-        }}
-      >
-        <Money name="A/C Balance" value="54,835"></Money>
-        <Money name="Total Properties" value="67,25,000"></Money>
-        <Money name="Total Investments" value="34,67,291"></Money>
-        <Money name="Total Golds" value="11,08,128"></Money>
-        <Money name="Total Loans" value="17,92,000"></Money>
-      </Box>
+      <PageTitle {...pageItems.dashboard}>
+        <Typography
+          sx={{
+            width: '100%',
+            fontSize: '3rem',
+            fontWeight: 100,
+            textAlign: 'right',
+            marginTop: -3,
+            letterSpacing: -2,
+            color: 'rgb(62, 80, 96)',
+          }}
+        >
+          You owned
+        </Typography>
+        <Typography
+          sx={{
+            width: '100%',
+            fontWeight: 600,
+            fontSize: '3rem',
+            marginTop: -3,
+            textAlign: 'right',
+            letterSpacing: -3,
+            color: '#1fa293',
+          }}
+        >
+          Rs.1,32,84,912.00
+        </Typography>
+      </PageTitle>
+      <MoneyTable list={moneyList} />
       <Box className="dashboard-page-wrapper">
         <MonthlyIncomeChart />
         <DailyChart />
