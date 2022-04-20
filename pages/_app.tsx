4713bc3e634c43a50b240f79e7fb826be8fdb2ef
mohaa <mohaa.r.6789@gmail.com>
2022-04-20 23:53:51 +0530

    code shorting at app theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,17 @@
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import '../styles/global.css'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import lightTheme from '@styles/lightTheme'
+import '../styles/global.scss'
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      '"IBM Plex Sans"',
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-})
-
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
       <Header />
       <main className="main-app-wrapper">
-        <Component {...pageProps} />
+        {/* <Component {...pageProps} /> */}
       </main>
       <Footer />
     </ThemeProvider>
