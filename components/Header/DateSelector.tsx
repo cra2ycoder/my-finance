0ae10e5cada231b9116b53abcdf4ddec184e3079
mohaa <mohaa.r.6789@gmail.com>
2022-04-21 23:10:14 +0530

    code correction in DateSelector

--- components/Header/DateSelector.tsx
+++ components/Header/DateSelector.tsx
@@ -11,9 +11,11 @@ import menuPageProps from './props'
 function DateSelector() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget)
   }
+
   const handleClose = () => {
     setAnchorEl(null)
   }
@@ -32,8 +34,8 @@ function DateSelector() {
       </Button>
       <Menu
         id="basic-menu"
-        anchorEl={anchorEl}
         open={open}
+        anchorEl={anchorEl}
         onClose={handleClose}
         {...menuPageProps}
       >
