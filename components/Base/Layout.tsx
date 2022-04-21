41ad53f165eb7e822701e2275a5a42dd70525f7d
mohaa <mohaa.r.6789@gmail.com>
2022-04-21 23:09:29 +0530

    FluidFlexBox comp created

--- components/Base/Layout.tsx
+++ components/Base/Layout.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Box from '@mui/material/Box'
 import styles from './layout.module.scss'
 
 function MainFlexBox({ children }: any = {}) {
   return <main className={styles.mainFlexBox}>{children}</main>
 }
 
-export { MainFlexBox }
+function FluidFlexBox({ children, ...rest }: any = {}) {
+  return (
+    <Box width="100%" display="flex" justifyContent="space-between" {...rest}>
+      {children}
+    </Box>
+  )
+}
+
+export { MainFlexBox, FluidFlexBox }
