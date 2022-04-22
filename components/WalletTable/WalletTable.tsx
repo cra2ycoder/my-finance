3ae412d72ec92ba3e0de7dab1d1144519571275c
mohaa <mohaa.r.6789@gmail.com>
2022-04-23 01:34:25 +0530

    gridbox integrated for wallettable

--- components/WalletTable/WalletTable.tsx
+++ components/WalletTable/WalletTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Box from '@mui/material/Box'
+import { GridBox } from '@components/Base/Layout'
 import WalletItem from './WalletItem'
 import { IWalletTableProps, IWalletItemProps } from './typings'
 
@@ -7,11 +7,15 @@ function WalletTable(props: IWalletTableProps) {
   const { list = [] } = props
 
   return (
-    <>
+    <GridBox
+      className="dashboard-graphs-wrapper"
+      columns={{ xl: 4, lg: 3, md: 2, sm: 1 }}
+      gap="0.5rem"
+    >
       {list.map((x: IWalletItemProps, id: number) => (
         <WalletItem {...x} key={`wallet-item-${id}`} />
       ))}
-    </>
+    </GridBox>
   )
 }
 
