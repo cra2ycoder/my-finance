cd6171343d5876c960149a284a861340c2a61b9f
mohaa <mohaa.r.6789@gmail.com>
2022-06-28 21:45:36 +0530

    typings moved

--- components/Base/typings.ts
+++ components/Base/typings.ts
@@ -7,3 +7,20 @@ export interface IMenuLinkProps {
   text: string
   color?: string
 }
+
+export interface IBreakpointProps {
+  xs?: number
+  sm?: number
+  md?: number
+  lg?: number
+  xl?: number
+}
+
+export interface IGridBoxProps {
+  children: React.ReactNode
+  columns: IBreakpointProps
+  breakpoints?: IBreakpointProps
+  sx?: any
+  gap?: number | string
+  [key: string]: any
+}
