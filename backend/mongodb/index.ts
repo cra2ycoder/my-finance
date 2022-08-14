44c5552f1d143d233c9c998cccc8ff52d26bbf38
mohaa <mohaa.r.6789@gmail.com>
2022-08-15 02:05:38 +0530

    mongodb server created

---
+++ backend/mongodb/index.ts
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+// calling this for accessing the NODE_ENV vars from .env file
+dotenv.config()
+
+const MONGODB_CLUSTER_URI = process.env.MONGODB_CLUSTER_URI
+
+if (!MONGODB_CLUSTER_URI) {
+  throw new Error(
+    'Please define the MONGODB_CLUSTER_URI environment variable inside .env'
+  )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { connection: null, promise: null }
+}
+
+async function connectMongoDB() {
+  if (cached) {
+    return cached.connection
+  }
+
+  if (!cached.promise) {
+    const mge = {
+      connection: null,
+      promise: null,
+    }
+
+    const options = {
+      bufferCommands: false,
+    }
+
+    mge.promise = mongoose
+      .connect(MONGODB_CLUSTER_URI, options)
+      .then(mongoose => mongoose)
+
+    mge.connection = await mge.promise
+    console.log({ mge })
+    cached = mge
+    return mge.connection
+  }
+}
+
+export default connectMongoDB
