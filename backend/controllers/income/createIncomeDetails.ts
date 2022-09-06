12a0e2291396218bfc68e0bc1ab77d7c4a1d7dc6
mohaa <mohaa.r.6789@gmail.com>
2022-09-07 01:31:06 +0530

    post & get api tried

--- backend/controllers/income/createIncomeDetails.ts
+++ backend/controllers/income/createIncomeDetails.ts
@@ -1,10 +1,30 @@
 import { Request, Response, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
+import mongoose from 'mongoose'
+import connectMongoDB from '../../mongodb'
+
+const WalletSchema = new mongoose.Schema({
+  total: {
+    type: 'Number',
+  },
+  lastUpdatedDate: {
+    type: 'String',
+  },
+})
+
+const IncomeSchema = new mongoose.Schema({
+  wallet: WalletSchema,
+})
+
+const IncomeModel =
+  mongoose.models.Income || mongoose.model('Income', IncomeSchema)
 
 export const createIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200)
+    await connectMongoDB()
 
-    res.json({ message: 'create a income details' })
+    const wallet = await IncomeModel.create(req.body)
+    // console.log({ wallet, IncomeModel, body: req.body })
+    res.status(201).json({ success: true, data: wallet })
   }
 )
