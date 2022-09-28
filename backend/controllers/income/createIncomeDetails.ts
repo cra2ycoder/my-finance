6b643369bce31806dc8edcb3dad8ef866ade1d51
mohaa <mohaa.r.6789@gmail.com>
2022-09-28 22:35:26 +0530

    url handling initiated for post req handling for income

--- backend/controllers/income/createIncomeDetails.ts
+++ backend/controllers/income/createIncomeDetails.ts
@@ -23,6 +23,18 @@ export const createIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     await connectMongoDB()
 
+    const originalURL = req.originalUrl
+    const urlObj = new URL(`https://mohaa.com${originalURL}`)
+    const searchParams = urlObj?.searchParams
+    const incomeType = searchParams.get('type')
+
+    console.log({ originalURL, urlObj, incomeType })
+
+    /**
+     * @todo
+     * - update the MongoDB Schema based on the new model
+     */
+
     const wallet = await IncomeModel.create(req.body)
     // console.log({ wallet, IncomeModel, body: req.body })
     res.status(201).json({ success: true, data: wallet })
