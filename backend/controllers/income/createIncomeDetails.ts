4c36b071fa8e241c0dff4aefe62a8e38d8213b6f
mohaa <mohaa.r.6789@gmail.com>
2022-09-29 23:34:24 +0530

    post implemented in base level

--- backend/controllers/income/createIncomeDetails.ts
+++ backend/controllers/income/createIncomeDetails.ts
@@ -2,40 +2,34 @@ import { Request, Response, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
 import mongoose from 'mongoose'
 import connectMongoDB from '../../mongodb'
-
-const WalletSchema = new mongoose.Schema({
-  total: {
-    type: 'Number',
-  },
-  lastUpdatedDate: {
-    type: 'String',
-  },
-})
-
-const IncomeSchema = new mongoose.Schema({
-  wallet: WalletSchema,
-})
-
-const IncomeModel =
-  mongoose.models.Income || mongoose.model('Income', IncomeSchema)
+import { IncomeModel } from './schema/income'
 
 export const createIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     await connectMongoDB()
 
-    const originalURL = req.originalUrl
-    const urlObj = new URL(`https://mohaa.com${originalURL}`)
-    const searchParams = urlObj?.searchParams
-    const incomeType = searchParams.get('type')
-
-    console.log({ originalURL, urlObj, incomeType })
-
     /**
      * @todo
-     * - update the MongoDB Schema based on the new model
+     * - store data based on the year and month
+     *
      */
 
+    // const originalURL = req.originalUrl
+    // const urlObj = new URL(`https://mohaa.com${originalURL}`)
+    // const searchParams = urlObj?.searchParams
+    // const incomeMonth = searchParams.get('month')
+    // const incomeYear = searchParams.get('year')
+
+    // console.log({ originalURL, urlObj, incomeType })
+
+    // const wallet = await IncomeModel.create({
+    //   2022: {
+    //     september: req.body,
+    //   },
+    // })
+
     const wallet = await IncomeModel.create(req.body)
+
     // console.log({ wallet, IncomeModel, body: req.body })
     res.status(201).json({ success: true, data: wallet })
   }
