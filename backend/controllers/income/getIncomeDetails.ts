d5a9a255c722be9bd98e58c5887b9fe4a838af56
mohaa <mohaa.r.6789@gmail.com>
2022-09-29 23:34:14 +0530

    get implemented in base level

--- backend/controllers/income/getIncomeDetails.ts
+++ backend/controllers/income/getIncomeDetails.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
+import { IncomeModel } from './schema/income'
 
 export const getIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     res.status(200)
-
-    res.json({})
+    const data = await IncomeModel.find()
+    res.json(data)
   }
 )
