dd12ee4eebd23abc4536ccc17cc853c340c8488a
mohaa <mohaa.r.6789@gmail.com>
2022-10-03 00:28:04 +0530

    creating the data based on the year

--- backend/controllers/income/createIncomeDetails.ts
+++ backend/controllers/income/createIncomeDetails.ts
@@ -2,35 +2,31 @@ import { Request, Response, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
 import mongoose from 'mongoose'
 import connectMongoDB from '../../mongodb'
-import { IncomeModel } from './schema/income'
+import { IncomeSchema } from './schema/income'
 
 export const createIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     await connectMongoDB()
 
-    /**
-     * @todo
-     * - store data based on the year and month
-     *
-     */
+    const curDate = new Date()
+    const curYear = curDate.getFullYear()
+    const curMonth = curDate.getMonth()
 
-    // const originalURL = req.originalUrl
-    // const urlObj = new URL(`https://mohaa.com${originalURL}`)
-    // const searchParams = urlObj?.searchParams
-    // const incomeMonth = searchParams.get('month')
-    // const incomeYear = searchParams.get('year')
+    const originalURL = req.originalUrl
+    const urlObj = new URL(`https://mohaa.com${originalURL}`)
+    const searchParams = urlObj?.searchParams
+    const incomeMonth = searchParams.get('month') || curMonth
+    const incomeYear = searchParams.get('year') || curYear
 
-    // console.log({ originalURL, urlObj, incomeType })
+    const IncomeModel =
+      mongoose.models.Income ||
+      mongoose.model('Income', IncomeSchema, incomeYear.toString())
 
-    // const wallet = await IncomeModel.create({
-    //   2022: {
-    //     september: req.body,
-    //   },
-    // })
+    const Wallet = await IncomeModel.create({
+      month: incomeMonth,
+      data: req.body,
+    })
 
-    const wallet = await IncomeModel.create(req.body)
-
-    // console.log({ wallet, IncomeModel, body: req.body })
-    res.status(201).json({ success: true, data: wallet })
+    res.status(201).json({ success: true, data: Wallet })
   }
 )
