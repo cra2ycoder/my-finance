416bade45f008f69c8d9da60f61f0d24eed9de45
mohaa <mohaa.r.6789@gmail.com>
2022-10-03 00:28:22 +0530

    get issue fix after collection change

--- backend/controllers/income/getIncomeDetails.ts
+++ backend/controllers/income/getIncomeDetails.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
-import { IncomeModel } from './schema/income'
+import mongoose from 'mongoose'
+// import { IncomeModel } from './schema/income'
 
 export const getIncomeDetails = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     res.status(200)
+
+    const IncomeModel = mongoose.models.Income
+
     const data = await IncomeModel.find()
     res.json(data)
   }
