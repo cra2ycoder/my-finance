d3c18ec29b72543ff7b3f16f81cd30cce4b41a34
mohaa <mohaa.r.6789@gmail.com>
2022-10-04 23:06:05 +0530

    connection checks and data creation

--- backend/controllers/income/createIncomeDetails.ts
+++ backend/controllers/income/createIncomeDetails.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from 'express'
-import asyncHandler from 'express-async-handler'
 import mongoose from 'mongoose'
+import asyncHandler from 'express-async-handler'
+import { Request, Response, NextFunction } from 'express'
 import connectMongoDB from '../../mongodb'
 import { IncomeSchema } from './schema/income'
 
@@ -15,18 +15,31 @@ export const createIncomeDetails = asyncHandler(
     const originalURL = req.originalUrl
     const urlObj = new URL(`https://mohaa.com${originalURL}`)
     const searchParams = urlObj?.searchParams
-    const incomeMonth = searchParams.get('month') || curMonth
+    const incomeMonth = searchParams.get('month') || curMonth + 1
     const incomeYear = searchParams.get('year') || curYear
 
     const IncomeModel =
       mongoose.models.Income ||
       mongoose.model('Income', IncomeSchema, incomeYear.toString())
 
-    const Wallet = await IncomeModel.create({
-      month: incomeMonth,
-      data: req.body,
-    })
-
-    res.status(201).json({ success: true, data: Wallet })
+    mongoose.connection.db
+      .listCollections({ name: incomeYear.toString() })
+      .next(async (err, collInfo) => {
+        if (collInfo === null) {
+          const Wallet = await IncomeModel.create({
+            month: incomeMonth,
+            ...req.body,
+          })
+          res.status(201).json({
+            success: true,
+            message: 'successfully created',
+            accounts: Wallet?.accounts || [],
+          })
+        } else {
+          res.status(403).json({
+            error: `${incomeYear} collection is already exists! use PUT method to update!`,
+          })
+        }
+      })
   }
 )
